daca1b6954777388ba65428fa2149fe8888db9b9
simanzler <simanzler@gmail.com>
2024-08-13 11:32:12 -0400

    Added tab button animation

--- src/components/navigation/TabBar.tsx
+++ src/components/navigation/TabBar.tsx
@@ -1,90 +1,77 @@
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { BottomTabBarProps } from '@react-navigation/bottom-tabs'
-import { useTheme } from '@/src/providers/ThemeProvider';
-import { useElevation } from '@/src/constants/Themes';
-import { Feather } from '@expo/vector-icons';
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
+import { useTheme } from "@/src/providers/ThemeProvider";
+import { useElevation } from "@/src/constants/Themes";
+import { Feather } from "@expo/vector-icons";
+import TabBarButton from "./TabBarButton";
 
 export function TabBar({ state, descriptors, navigation }: BottomTabBarProps) {
-    const { theme } = useTheme();
+  const { theme } = useTheme();
 
-    const icon: Record<any, (props: any) => JSX.Element> = {
-        index: (props: any) => <Feather name='home' size={24} color={theme.text} {...props} />,
-        explore: (props: any) => <Feather name='compass' size={24} color={theme.text} {...props} />,
-        profile: (props: any) => <Feather name='user' size={24} color={theme.text} {...props} />
-    }
+  return (
+    <View
+      style={[
+        styles.container,
+        { backgroundColor: theme.background },
+        useElevation(7, theme),
+      ]}
+    >
+      {state.routes.map((route: any, index: any) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
 
-    return (
-        <View style={[styles.container, { backgroundColor: theme.background }, useElevation(7, theme)]}>
-            {state.routes.map((route: any, index: any) => {
-                const { options } = descriptors[route.key];
-                const label =
-                    options.tabBarLabel !== undefined
-                        ? options.tabBarLabel
-                        : options.title !== undefined
-                            ? options.title
-                            : route.name;
+        const isFocused = state.index === index;
 
-                const isFocused = state.index === index;
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
 
-                const onPress = () => {
-                    const event = navigation.emit({
-                        type: 'tabPress',
-                        target: route.key,
-                        canPreventDefault: true,
-                    });
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
 
-                    if (!isFocused && !event.defaultPrevented) {
-                        navigation.navigate(route.name, route.params);
-                    }
-                };
+        const onLongPress = () => {
+          navigation.emit({
+            type: "tabLongPress",
+            target: route.key,
+          });
+        };
 
-                const onLongPress = () => {
-                    navigation.emit({
-                        type: 'tabLongPress',
-                        target: route.key,
-                    });
-                };
-
-                return (
-                    <TouchableOpacity
-                        key={route.name}
-                        accessibilityRole="button"
-                        accessibilityState={isFocused ? { selected: true } : {}}
-                        accessibilityLabel={options.tabBarAccessibilityLabel}
-                        testID={options.tabBarTestID}
-                        onPress={onPress}
-                        onLongPress={onLongPress}
-                        style={styles.item}
-                    >
-                        {icon[route.name]({
-                            color: isFocused ? theme.primary : theme.text
-                        })}
-                        <Text style={{ color: isFocused ? theme.primary : theme.text }}>
-                            {label}
-                        </Text>
-                    </TouchableOpacity>
-                );
-            })}
-        </View>
-    );
+        return (
+          <TabBarButton
+            key={route.name}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            isFocused={isFocused}
+            routeName={route.name}
+            color={isFocused ? theme.primary : theme.text}
+            label={label}
+          />
+        );
+      })}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        position: 'absolute',
-        bottom: 50,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        alignSelf: 'center',
-        width: 250,
-        paddingVertical: 12,
-        borderRadius: 35
-    },
-    item: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: 5,
-    }
-})
+  container: {
+    position: "absolute",
+    bottom: 50,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    alignSelf: "center",
+    width: 250,
+    paddingVertical: 12,
+    borderRadius: 35,
+  },
+});
