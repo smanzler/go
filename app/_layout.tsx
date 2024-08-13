950f81947a1a15242e53accd8b67a50c9c86d22f
simanzler <simanzler@gmail.com>
2024-08-13 13:28:23 -0400

    Added loading state variable to auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,74 +1,75 @@
-import { useFonts } from 'expo-font';
-import { Redirect, router, Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { useFonts } from "expo-font";
+import { Redirect, router, Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
-import AuthProvider, { useAuth } from '@/src/providers/AuthProvider';
-import { ThemeProvider, useTheme } from '@/src/providers/ThemeProvider';
-import { useElevation } from '@/src/constants/Themes';
-import tinycolor from 'tinycolor2';
+import AuthProvider, { useAuth } from "@/src/providers/AuthProvider";
+import { ThemeProvider, useTheme } from "@/src/providers/ThemeProvider";
+import { useElevation } from "@/src/constants/Themes";
+import tinycolor from "tinycolor2";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-    const [loaded] = useFonts({
-        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    });
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
 
-    useEffect(() => {
-        if (loaded) {
-            SplashScreen.hideAsync();
-        }
-    }, [loaded]);
-
-    if (!loaded) {
-        return null;
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
     }
+  }, [loaded]);
 
-    return (
-        <AuthProvider>
-            <ThemeProvider>
-                <RootLayoutNav />
-            </ThemeProvider>
-        </AuthProvider>
-    );
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <AuthProvider>
+      <ThemeProvider>
+        <RootLayoutNav />
+      </ThemeProvider>
+    </AuthProvider>
+  );
 }
 
 function RootLayoutNav() {
-    const { isAuthenticated } = useAuth();
-    const { theme } = useTheme();
+  const { isAuthenticated, isLoading } = useAuth();
+  const { theme } = useTheme();
 
-    useEffect(() => {
-        if (!isAuthenticated) { 
-            router.navigate('/(auth)/login');
-        }
-    }, [isAuthenticated])
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) {
+      router.navigate("/(auth)/login");
+    }
+  }, [isAuthenticated, isLoading]);
 
-    return (
-        <Stack>
-            <Stack.Screen
-                name='(tabs)'
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name='(modals)/task'
-                options={{
-                    presentation: 'transparentModal',
-                    animation: 'fade',
-                    headerShown: false,
-                }}
-            />
-            <Stack.Screen 
-                name='(auth)/login'
-                options={{
-                    presentation: 'modal',
-                    headerTitle: 'Login',
-                    headerTitleStyle: { color: theme.text},
-                    headerStyle: { backgroundColor: tinycolor(theme.background).lighten(10).toHexString() }
-                }}
-            />
-        </Stack>
-    )
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="(modals)/task"
+        options={{
+          presentation: "transparentModal",
+          animation: "fade",
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="(auth)/login"
+        options={{
+          presentation: "modal",
+          headerTitle: "Login",
+          headerTitleStyle: { color: theme.text },
+          headerStyle: {
+            backgroundColor: tinycolor(theme.background)
+              .lighten(10)
+              .toHexString(),
+          },
+        }}
+      />
+    </Stack>
+  );
 }
