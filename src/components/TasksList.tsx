5347d19536b621a3274214ed4a10699dc4163809
simanzler <simanzler@gmail.com>
2024-08-15 20:20:18 -0500

    Changed to using map instead of flatlist

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -1,24 +1,18 @@
-import { FlatList } from "react-native";
+import { FlatList, View } from "react-native";
 import TaskListItem from "./TaskListItem";
 import { useEffect, useState } from "react";
 import { tasksCollection } from "../db";
 import Task from "../models/Task";
 
 import { withObservables } from "@nozbe/watermelondb/react";
-import { SwipeListView } from "react-native-swipe-list-view";
-import HiddenItemWithActions from "./HiddenItemWithActions";
 
 function TasksList({ tasks }: { tasks: Task[] }) {
   return (
-    <SwipeListView
-      style={{ overflow: "visible" }}
-      data={tasks}
-      renderItem={({ item }) => <TaskListItem task={item} />}
-      renderHiddenItem={({ item }) => <HiddenItemWithActions task={item} />}
-      disableRightSwipe
-      rightOpenValue={-70}
-      nestedScrollEnabled
-    />
+    <View>
+      {tasks.map((task) => (
+        <TaskListItem key={task.id} task={task} />
+      ))}
+    </View>
   );
 }
 
