2548bc734d3982cd23e9265c5d69e79c7d49bcd3
simanzler <simanzler@gmail.com>
2024-08-18 11:11:36 -0500

    Added reanimated-color-picker

--- app/(settings)/theme.tsx
+++ app/(settings)/theme.tsx
@@ -1,16 +1,49 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { useTheme } from "@/src/providers/ThemeProvider";
+import React, { useState } from "react";
+import { Button, Modal, StyleSheet, View } from "react-native";
 
-const Theme = () => {
-  const { theme } = useTheme();
+import ColorPicker, {
+  Panel1,
+  Swatches,
+  Preview,
+  OpacitySlider,
+  HueSlider,
+} from "reanimated-color-picker";
+
+export default function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  // Note: 👇 This can be a `worklet` function.
+  const onSelectColor = ({ hex }: { hex: string }) => {
+    // do something with the selected color.
+    console.log(hex);
+  };
+
   return (
-    <View style={{ flex: 1, backgroundColor: theme.background }}>
-      <Text>Theme</Text>
+    <View style={styles.container}>
+      <Button title="Color Picker" onPress={() => setShowModal(true)} />
+
+      <Modal visible={showModal} animationType="slide">
+        <ColorPicker
+          style={{ width: "70%" }}
+          value="red"
+          onComplete={onSelectColor}
+        >
+          <Preview />
+          <Panel1 />
+          <HueSlider />
+          <OpacitySlider />
+          <Swatches />
+        </ColorPicker>
+
+        <Button title="Ok" onPress={() => setShowModal(false)} />
+      </Modal>
     </View>
   );
-};
+}
 
-export default Theme;
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
