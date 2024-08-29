3ab4b1fc6ba5c6892fe681723f468a6dc4414699
simanzler <simanzler@gmail.com>
2024-08-28 23:19:16 -0400

    finally figured out animating text input text and fixed type error

--- src/components/TaskListItem.tsx
+++ src/components/TaskListItem.tsx
@@ -1,4 +1,12 @@
-import { StyleSheet, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  NativeSyntheticEvent,
+  StyleSheet,
+  TextInput,
+  TextInputChangeEventData,
+  TextInputProps,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { ThemedText } from "./ThemedText";
 import { ThemedButton } from "./ThemedButton";
 import Task from "../models/Task";
@@ -8,14 +16,20 @@ import database from "../db";
 import { useTheme } from "../providers/ThemeProvider";
 import { useElevation } from "../constants/Themes";
 import { Entypo, Feather, FontAwesome6, Ionicons } from "@expo/vector-icons";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Animated, {
+  Easing,
   FadeInLeft,
   FadeOutLeft,
   interpolate,
+  interpolateColor,
   LinearTransition,
+  runOnJS,
+  useAnimatedProps,
+  useAnimatedReaction,
   useAnimatedStyle,
   useSharedValue,
+  withTiming,
 } from "react-native-reanimated";
 import { usePanXGesture } from "../hooks/usePanXGesture";
 import { GestureDetector } from "react-native-gesture-handler";
@@ -26,13 +40,38 @@ export type TaskListItem = {
 
 function TaskListItem({ task }: TaskListItem) {
   const { theme } = useTheme();
-  const [description, setDescription] = useState(task.description);
+  const descriptionSharedValue = useSharedValue<string>(task.description);
+  const [animating, setAnimating] = useState(false);
+
+  const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
+  const AnimatedIcon = Animated.createAnimatedComponent(Feather);
+
   const taskCompleteSharedValue = useSharedValue(task.complete);
+  const progress = useSharedValue(task.complete ? 1 : 0);
+  const textColor = useSharedValue(theme.text);
 
   useEffect(() => {
-    setDescription(task.description);
     taskCompleteSharedValue.value = task.complete;
   }, [task, task.description, task.complete]);
+
+  const fillerAnimatedStyles = useAnimatedStyle(() => {
+    const width = interpolate(progress.value, [0, 1], [0, 100]);
+    return {
+      width: `${width}%`,
+    };
+  });
+
+  const colorAnimatedStyles = useAnimatedStyle(() => {
+    const color = interpolateColor(
+      progress.value,
+      [0, 1],
+      [textColor.value, "rgba(0, 0, 0, 0.85)"]
+    );
+
+    return {
+      color,
+    };
+  }, [textColor.value]);
 
   const onDelete = async () => {
     await database.write(async () => {
@@ -41,6 +80,16 @@ function TaskListItem({ task }: TaskListItem) {
   };
 
   const onComplete = async () => {
+    setAnimating(true);
+
+    progress.value = withTiming(
+      progress.value === 0 ? 1 : 0,
+      { duration: 1000, easing: Easing.out(Easing.exp) },
+      () => {
+        runOnJS(setAnimating)(false);
+      }
+    );
+
     await database.write(async () => {
       await task.update((task) => {
         task.complete = !task.complete;
@@ -49,10 +98,11 @@ function TaskListItem({ task }: TaskListItem) {
   };
 
   const onBlur = async () => {
-    if (task.description === description) return;
+    console.log(descriptionSharedValue.value);
+    if (task.description === descriptionSharedValue.value) return;
     await database.write(async () => {
       await task.update((task) => {
-        task.description = description;
+        task.description = descriptionSharedValue.value;
       });
     });
   };
@@ -92,7 +142,7 @@ function TaskListItem({ task }: TaskListItem) {
           ? "grey"
           : "#50C878"
         : offsetX.value < 0
-        ? "#DC143C"
+        ? "#ff4444"
         : "transparent";
 
     return { backgroundColor: bgColor };
@@ -107,6 +157,12 @@ function TaskListItem({ task }: TaskListItem) {
       ],
     };
   }, []);
+
+  const animatedProps = useAnimatedProps(() => {
+    return {
+      text: descriptionSharedValue.value,
+    };
+  });
 
   return (
     <Animated.View
@@ -135,26 +191,27 @@ function TaskListItem({ task }: TaskListItem) {
             panXAnimatedStyles,
           ]}
         >
-          <TextInput
-            style={[
-              styles.taskText,
-              { color: theme.text },
-              task.complete && styles.completedText,
-            ]}
+          <Animated.View style={[styles.filler, fillerAnimatedStyles]} />
+
+          <AnimatedTextInput
+            style={[styles.taskText, colorAnimatedStyles]}
             multiline
             blurOnSubmit
             scrollEnabled={false}
             onBlur={onBlur}
-            editable={!task.complete}
+            editable={!task.complete && !animating}
             returnKeyType="done"
-            value={description}
-            onChangeText={setDescription}
+            defaultValue={descriptionSharedValue.value}
+            onChangeText={(text) => {
+              descriptionSharedValue.value = text;
+            }}
+            animatedProps={animatedProps as any}
           />
           <View style={styles.buttonsContainer}>
-            <TouchableOpacity onPress={onComplete}>
-              <Feather
+            <TouchableOpacity onPress={onComplete} disabled={animating}>
+              <AnimatedIcon
                 name={task.complete ? "check-circle" : "circle"}
-                color={theme.text}
+                style={colorAnimatedStyles}
                 size={24}
               />
             </TouchableOpacity>
@@ -176,28 +233,25 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     borderRadius: 10,
   },
+  filler: {
+    borderRadius: 10,
+    position: "absolute",
+    height: "100%",
+    backgroundColor: "#50C878",
+  },
   taskContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: "center",
 
-    padding: 10,
     borderRadius: 10,
   },
   taskText: {
+    flex: 1,
     fontSize: 16,
-    width: "90%",
-  },
-  completedText: {
-    textDecorationLine: "line-through",
-    color: "gray",
+    margin: 10,
   },
   buttonsContainer: {
-    flexDirection: "row",
-    gap: 10,
-  },
-  completeBtn: {
-    marginRight: 10,
+    margin: 10,
   },
   deleteBtn: {
     backgroundColor: "red",
